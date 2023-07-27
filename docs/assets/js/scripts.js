f1f8cde1774a0b1341dfe84d6f583635c82f2a1f
heds-front-end <35922709+Edson-PHP@users.noreply.github.com>
2023-07-27 01:02:30 -0300

    Update scripts.js
    Adicionei a função em Javascript da funcionalidade do botão Up.

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -6,4 +6,23 @@ searchButton.addEventListener('click', (e) => {
   const searchValue = searchInput.value;
   const url = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(searchValue)}.md`; 
   window.open(url, '_blank');
-})
+});
+
+// Botão Up de Scroll
+const moveTop = () => {
+  scrollTo({
+    behavior: "smooth",
+    top: 0
+  });
+}
+
+window.addEventListener("scroll", () => {
+  if(scrollY <= 50){
+    btnUp.style.display = "none";
+  } else { 
+    btnUp.style.display = "block";
+  }
+});
+
+const btnUp = document.querySelector(".up");
+btnUp.addEventListener("click", moveTop);
